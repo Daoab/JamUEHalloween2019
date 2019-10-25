64a3cd5c8c6062f4d0781a6263fcbd6ec76cc2db
Franpok <Franpok@users.noreply.github.com>
2019-10-25 23:01:38 +0200

    Script de movimiento del personaje

---
+++ JamUEHalloween2019/Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-speed, 0f, 0f) * Time.deltaTime; //Movimiento a la izquierda
+            transform.forward = Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(speed, 0f, 0f) * Time.deltaTime; //Movimiento a la derecha
+            transform.forward = Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0f, 0f, speed) * Time.deltaTime; //Movimiento hacia delante
+            transform.forward = Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0f, 0f, -speed) * Time.deltaTime; //Movimiento hacia atras
+            transform.forward = Vector3.back;
+
+        }
+    }
+}
