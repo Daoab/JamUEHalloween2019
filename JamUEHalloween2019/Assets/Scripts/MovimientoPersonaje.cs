84889340e6c6fcaf839c0b4128c1fc3c9818da4d
Franpok <Franpok@users.noreply.github.com>
2019-10-26 03:55:09 +0200

    Animaciones, creo que se puede subir así no me hagais mucho caso, si falla avisadme

--- JamUEHalloween2019/Assets/Scripts/MovimientoPersonaje.cs
+++ JamUEHalloween2019/Assets/Scripts/MovimientoPersonaje.cs
@@ -4,40 +4,55 @@ using UnityEngine;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
+    Animator anim;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
+        anim = this.gameObject.GetComponent<Animator>();
+        anim.gameObject.SetActive(true);
         speed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        stop();
         movement();
     }
+    void stop()
+    {
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("walk", false);
 
+        }
+    }
     void movement()
     {
         if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position + new Vector3(-speed, 0f, 0f) * Time.deltaTime; //Movimiento a la izquierda
             transform.forward = Vector3.left;
+            anim.SetBool("walk", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = transform.position + new Vector3(speed, 0f, 0f) * Time.deltaTime; //Movimiento a la derecha
             transform.forward = Vector3.right;
+            anim.SetBool("walk", true);
         }
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + new Vector3(0f, 0f, speed) * Time.deltaTime; //Movimiento hacia delante
             transform.forward = Vector3.forward;
+            anim.SetBool("walk", true);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position = transform.position + new Vector3(0f, 0f, -speed) * Time.deltaTime; //Movimiento hacia atras
             transform.forward = Vector3.back;
+            anim.SetBool("walk", true);
 
         }
     }
