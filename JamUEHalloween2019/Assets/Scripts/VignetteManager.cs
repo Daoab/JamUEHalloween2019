621a5a43d0474e7d10d30f8f0383dfcb320423ee
Daoab <38359000+Daoab@users.noreply.github.com>
2019-10-26 15:45:49 +0200

    Primera versión viñeta

---
+++ JamUEHalloween2019/Assets/Scripts/VignetteManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class VignetteManager : MonoBehaviour
+{
+    [SerializeField] float timeBetweenVignetteReductions = 1.0f;
+    [SerializeField] float vigentteIncrementPerTick = 0.1f;
+    [SerializeField] float startinVignette = 0.2f;
+
+    [SerializeField] PostProcessProfile postProcessingProfile;
+    
+    float incrementedIntensity = 0.0f;
+    Vignette vignette;
+    
+    void Start()
+    {
+        postProcessingProfile.TryGetSettings<Vignette>(out vignette);
+        vignette.intensity.Override(startinVignette);
+
+        vignette.enabled.Override(true);
+
+        StartCoroutine(IncreaseVignette());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator IncreaseVignette()
+    {
+        yield return new WaitForSecondsRealtime(timeBetweenVignetteReductions);
+        incrementedIntensity += vigentteIncrementPerTick;
+        vignette.intensity.Override(incrementedIntensity);
+        if(incrementedIntensity < 1f)
+            StartCoroutine(IncreaseVignette());
+    }
+}
