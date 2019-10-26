c7af4a383299e990a330c66b097aed9dfd66e0ef
Franpok <Franpok@users.noreply.github.com>
2019-10-26 20:59:12 +0200

    Reloj fartusco

---
+++ JamUEHalloween2019/Assets/Scripts/Penpendulo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penpendulo : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        anim.SetBool("meneo", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
