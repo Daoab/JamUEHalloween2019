e77adf034dd9a1d34b70a9a7c74b50808fe15f1f
Franpok <Franpok@users.noreply.github.com>
2019-10-26 11:49:12 +0200

    Menú de inicio completo

--- JamUEHalloween2019/Assets/Scripts/EscenaInicial.cs
+++ JamUEHalloween2019/Assets/Scripts/EscenaInicial.cs
@@ -7,6 +7,7 @@ public class EscenaInicial : MonoBehaviour
 {
     public bool juego;
     public bool controles;
+    public bool salir;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,10 @@ public class EscenaInicial : MonoBehaviour
         {
             Debug.Log("JJoder");
             SceneManager.LoadScene(2);
+        }
+        if (salir)
+        {
+            Application.Quit();
         }
     }
 }
