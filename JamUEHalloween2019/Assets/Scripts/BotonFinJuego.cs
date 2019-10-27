c1ee61b0af7666055fde809e35ce3d45c4bf8978
Franpok <Franpok@users.noreply.github.com>
2019-10-27 07:05:06 +0100

    Base para las escenas de después del juego creadas

---
+++ JamUEHalloween2019/Assets/Scripts/BotonFinJuego.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonFinJuego : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void irACreditos()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void enterBoton()
+    {
+        transform.localScale = new Vector3(1.2f, 1.2f);
+    }
+    public void salirBoton()
+    {
+        transform.localScale = new Vector3(1f, 1f);
+    }
+}
